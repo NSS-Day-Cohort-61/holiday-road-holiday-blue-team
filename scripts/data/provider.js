3679da6a9308ef5acebe7bbb9a1794c3ac0ac84b
Marek Snyder <mr.snyder31@gmail.com>
2023-01-05 15:43:43 -0500

    added fetch functions

--- scripts/data/provider.js
+++ scripts/data/provider.js
@@ -1,6 +1,84 @@
-const apiURL = "http://localhost:8089";
-const applicationElement = document.querySelector("#holidayRoad");
+import apiKeys from "./Settings.js"
 
 const applicationState = {
-  itineraries: []
+  itineraries: [],
+  parks: [],
+  attractions: [],
+  eateries:[]
+}
+
+export const fetchParks = () => {
+    return fetch (`https://developer.nps.gov/api/v1/parks?api_key=${apiKeys.npsKey}`)
+    .then(response => response.json())
+    .then(
+        (servicePosts) => {
+            // Store the external state in application state
+            applicationState.parks = servicePosts
+        }
+        )
+}
+
+export const getParks = () => {
+    return applicationState.parks.map(park => ({...park}))
+}
+
+export const fetchAttractions = () => {
+    return fetch (`http://holidayroad.nss.team/bizarreries`)
+    .then(response => response.json())
+    .then(
+        (servicePosts) => {
+            // Store the external state in application state
+            applicationState.attractions = servicePosts
+        }
+        )
+}
+
+export const getAttractions = () => {
+    return applicationState.attractions.map(biz => ({...biz}))
+}
+
+export const fetchEateries = () => {
+    return fetch (`http://holidayroad.nss.team/eateries`)
+    .then(response => response.json())
+    .then(
+        (servicePosts) => {
+            // Store the external state in application state
+            applicationState.eateries = servicePosts
+        }
+        )
+}
+
+export const getEateries = () => {
+    return applicationState.eateries.map(eat => ({...eat}))
+}
+
+export const fetchWeather = () => {
+    return fetch (`api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apiKeys.weatherKey}`)
+    .then(res => res.json())
+    .then(
+        (data) => {
+            applicationState.weather = data
+        }
+    )
+}
+
+export const getWeather = () => {
+    return applicationState.weather.map(forecast => ({...forecast}))
+}
+
+export const saveItinerary = (input) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(input)
+    }
+
+
+    return fetch(`http://localhost:8088/itineraries`, fetchOptions)
+        .then(response => response.json())
+        .then((data) => {
+          applicationState.itineraries = data
+        })
 }
